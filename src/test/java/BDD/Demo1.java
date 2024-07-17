55c8b8e2e8f287c10336f682595ace3531c0dae3
Pranav <Pranav@DESKTOP-909HPG1>
2024-07-17 21:21:15 +0530

    BDD is Created by TL

---
+++ src/test/java/BDD/Demo1.java
@@ -0,0 +1,11 @@
+package BDD;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("BDD is Created By Team Lead");
+
+	}
+
+}
